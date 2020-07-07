0d1e80b6a642a25d10540f4ef3d565a6bb44f8d1
Gary Kang <garyzkang@gmail.com>
2020-07-07 12:07:16 -0400

    Add react-big-calendar and moment packages. Add NYY json object schedule

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+
+import "./App.css";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
+import nyy from "./nyy.json";
+
+const nyySchedule = nyy.map((game) => {
+  const startMoment = moment(game["START DATE"] + " " + game["START TIME"]);
+  const endMoment = moment(game["END DATE"] + " " + game["END TIME"]);
+
+  return {
+    start: startMoment.toDate(),
+    end: endMoment.toDate(),
+    title: game["SUBJECT"],
+  };
+});
+
+const localizer = momentLocalizer(moment);
+
+class App extends React.Component {
+  
+  state = {
+    events: nyySchedule,
+  };
+
+  render = () => {
+    return (
+      <div className="App">
+        <Calendar
+          localizer={localizer}
+          defaultDate={new Date()}
+          defaultView="month"
+          events={this.state.events}
+          style={{ height: "100vh" }}
+        />
+      </div>
+    );
+  };
 }
 
 export default App;
